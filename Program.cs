166907f8846f3f6fe66523e8bba5160d881340d8
its julii <nanji828@gmail.com>
2023-01-21 12:35:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double clothingPrice = double.Parse(Console.ReadLine());
+
+            double decor = budget * 0.1;
+            double clothing = statists * clothingPrice;
+
+            if (statists >= 150)
+            {
+                clothing = clothing - clothing * 0.1;
+            }
+            double neededMoney = clothing + decor;
+            double difference = budget - neededMoney;
+
+            if (difference >= 0)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {difference:F2} leva left.");
+            }
+            else
+            {
+                difference = Math.Abs(difference);
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {difference:F2} leva more.");
+            }
+            {
+
+            }
+
+        }
+    }
+}
+
